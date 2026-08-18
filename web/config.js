818f5ab857575728794736eb599be246826a9132
emcmp <130682343+emcmp@users.noreply.github.com>
2026-08-18 17:42:30 -0400

    Fix Z03 site configuration

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 const config = {
     nom: "Introduction à la programmation (TIM)",
-    description: "Site de référence du cours 905 - Introduction à la programmation (TIM)",
-    nomUrl: "905-IntroProg"
+    description: "Site de référence du cours Z03 - Introduction à la programmation Web",
+    nomUrl: "z03"
 }
 
 module.exports = config;
