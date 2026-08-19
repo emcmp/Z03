6ef75006b081f4ec15a338c5c0c157190feb0872
emcmp <130682343+emcmp@users.noreply.github.com>
2026-08-18 23:45:49 -0400

    Add temporary refonte documentation sidebar

---
+++ web/sidebars-refonte.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  refonte: [
+    {
+      type: "doc",
+      id: "REFONTE",
+      label: "Vue d'ensemble",
+    },
+    {
+      type: "doc",
+      id: "COMPETENCES_HTML_CSS",
+      label: "Compétences HTML/CSS",
+    },
+    {
+      type: "doc",
+      id: "EVALUATION",
+      label: "Évaluation progressive",
+    },
+    {
+      type: "doc",
+      id: "SUIVI_CONTENU",
+      label: "Suivi du contenu",
+    },
+  ],
+};
+
+module.exports = sidebars;
